05cc2a364e629245cc978b4fd0750eb54563ec39
rysyaa <captain0demon@gmail.com>
2024-01-20 22:19:26 +0600

    delete

--- src/App.js
+++ src/App.js
@@ -8,25 +8,41 @@ const App = (props) => {
     const { data } = await axios(API);
     setChar(data);
   }
+  console.log(char);
   useEffect(() => {
     readCharaters();
-  }, []);
+  },[]);
+  const Delete = async (id) => {
+    try {
+      await axios.delete(`${API}/${id}`);
+      readCharaters();
+    } catch (error) {
+      console.error("Ошибка при удалении персонажа:", error);
+    }
+  }
   // console.log(char[0]);
   return (
-    <div className="container">
-      {char[0]?.map((elem) => (
-        <div className="card" key={elem.id}>
-          <div className="card__img">
-            <img src={elem.image} alt="#" />
-          </div>
-          <div className="card__info">
-            <h2 className="card__title">{elem.name}</h2>
-            <p>{elem.status}</p>
-            <p>{elem.species}</p>
-            <p>{elem.gender}</p>
-          </div>
-        </div>
-      ))}
+    <div className="wrapper">
+      <div className="container">
+        {Array.isArray(char) ? (
+          char.map((elem) => (
+            <div className="card" key={elem.id}>
+              <div className="card__img">
+                <img src={elem.image} alt="#" />
+              </div>
+              <div className="card__info">
+                <h2 className="card__title">{elem.name}</h2>
+                <p>{elem.status}</p>
+                <p>{elem.species}</p>
+                <p>{elem.gender}</p>
+                <button onClick={() => Delete(elem.id)} className="btn__Delete">Delete</button>
+              </div>
+            </div>
+          ))
+        ) : (
+          <p>No characters available</p>
+        )}
+      </div>
     </div>
   );
 };
